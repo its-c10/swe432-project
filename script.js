3dd76a8a82e91615ceef6ab2b4e9b49db886b643
Caleb <caleb.ja.owens@gmail.com>
2023-10-18 08:40:01 -0400

    lrycs modal works. Working on adding forms to page

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const HIDE_SHARE_SLIDER_CLASS = "hide-slider"
+const HIDE_SHARE_SLIDER_CLASS = "hide-slider";
+const HIDE_LYRICS_MODAL_CLASS = 'hide-lyrics';
 window.addEventListener('load', function() {
     document.querySelector('.share-btn').addEventListener('click', (e) => {
         document.querySelector('.share-slider').classList.toggle(HIDE_SHARE_SLIDER_CLASS);
     });
-
-   
-
 });
 
-function showLyrics () {
-    console.log("Testing");
+function toggleLyrics () {
+    let lyricsModal = document.querySelector('.lyrics-modal');
+    let style = getComputedStyle(lyricsModal);
+    if (style.visibility == "hidden") {
+        lyricsModal.style.visibility = 'visible';
+    }else {
+        lyricsModal.style.visibility = 'hidden';
+    }
 }
