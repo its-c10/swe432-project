d56d726263d73b735f0cee70ca5eb748291ab1d8
Caleb <caleb.ja.owens@gmail.com>
2023-11-01 16:56:30 -0400

    Links all work and templates are functioning properly.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,30 +1,7 @@
 const HIDE_SHARE_SLIDER_CLASS = "hide-slider";
 const HIDE_LYRICS_MODAL_CLASS = 'hide-lyrics';
 
-let allRadioHosts = [
-    {
-        name: "Caleb",
-        favorite_song: "A Beautiful Lie",
-        favorite_song_artist: "Three Days Grace",
-        hiring_date: new Date(),
-        age: 22,
-        timesHosted: 1
-    },
-    {
-        name: "John",
-        favorite_song: "American Idiot",
-        favorite_song_artist: "Green Day",
-        hiring_date: new Date(),
-        age: 21,
-        timesHosted: 0
-    }
-]
 
-let currentRadioHost = allRadioHosts[0];
-let endTime = new Date();
-endTime.setHours(endTime.getHours() + 1);
-currentRadioHost.endTime = endTime; // added new property
-currentRadioHost.timesHosted += 1; // modified property
 console.log('Host ' + currentRadioHost.name + ' is on until ' + currentRadioHost.endTime); // accessed property
 
 /* Upon loading the web page, add an event listener that listens for the clicking of the share button */
