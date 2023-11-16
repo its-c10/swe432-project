5728d92691c5cb17de99879b3aa297173da4cccd
its-c10 <caleb.ja.owens@gmail.com>
2023-11-16 14:36:37 -0500

    got the mongoose connection to work

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 const app = express();
 const uri = "mongodb+srv://calebjaowens:2uoIa1DYyeCxce2g@swe432-project.2n5dvs0.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }
-  run().catch(console.dir);
+mongoose.connect(uri);
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('Connected to mongo');
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
