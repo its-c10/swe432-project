4fe8978622cd3804b994a12dc038f3f952cbc550
Caleb <caleb.ja.owens@gmail.com>
2023-11-17 23:04:11 -0500

    individual pages finally work

--- app.js
+++ app.js
@@ -66,29 +66,3 @@ function setupExpress() {
         console.log('Listening on port 8080');
     });
 }
-
-let allRadioHosts = [
-    {
-        name: "Caleb",
-        favorite_song: "A Beautiful Lie",
-        favorite_song_artist: "Three Days Grace",
-        hiring_date: new Date(),
-        age: 22,
-        timesHosted: 1
-    },
-    {
-        name: "John",
-        favorite_song: "American Idiot",
-        favorite_song_artist: "Green Day",
-        hiring_date: new Date(),
-        age: 21,
-        timesHosted: 0
-    }
-]
-
-let currentRadioHost = allRadioHosts[0];
-let endTime = new Date();
-endTime.setHours(endTime.getHours() + 1);
-currentRadioHost.endTime = endTime; // added new property
-currentRadioHost.timesHosted += 1; // modified property
-
